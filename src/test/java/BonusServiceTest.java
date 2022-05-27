f62209c58e16fd3e251aaef456957b4a0b940c71
Masha Rodionova <rodionovamv@gmai.com>
2022-05-27 13:42:51 +0300

    Add new test case

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -7,9 +7,9 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 100;
-        boolean registered = false;
-        long expected = 1;
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -58,6 +58,70 @@ public class BonusServiceTest {
         long amount = 1_000_000;
         boolean registered = false;
         long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateNotRegisteredAndEquallyLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 50000;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateRegisteredAndNotIntegerBonus() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 2978;
+        boolean registered = true;
+        long expected = 89;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateRegisteredAndNullAmount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 0;
+        boolean registered = true;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateRegisteredAndNegativeAmount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = -1000;
+        boolean registered = true;
+        long expected = -30;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
